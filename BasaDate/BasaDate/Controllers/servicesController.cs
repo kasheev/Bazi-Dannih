a7790e71900721cf31ed6186a791aa2816dba36e
Кащеев Илья <tactical949@gmail.com>
2020-06-06 00:19:15 +0600

    все нахуй новое сделал!!!!!!!!!!!!!

---
+++ BasaDate/BasaDate/Controllers/servicesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BasaDate.Models;
+
+namespace BasaDate.Controllers
+{
+    public class servicesController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: services
+        public ActionResult Index()
+        {
+            return View(db.services.ToList());
+        }
+
+        // GET: services/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service service = db.services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // GET: services/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: services/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,title,description")] service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.services.Add(service);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(service);
+        }
+
+        // GET: services/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service service = db.services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: services/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,title,description")] service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service);
+        }
+
+        // GET: services/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service service = db.services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            service service = db.services.Find(id);
+            db.services.Remove(service);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BasaDate/BasaDate/Controllers/softwaresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BasaDate.Models;
+
+namespace BasaDate.Controllers
+{
+    public class softwaresController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: softwares
+        public ActionResult Index()
+        {
+            return View(db.softwares.ToList());
+        }
+
+        // GET: softwares/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            software software = db.softwares.Find(id);
+            if (software == null)
+            {
+                return HttpNotFound();
+            }
+            return View(software);
+        }
+
+        // GET: softwares/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: softwares/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,os,programs")] software software)
+        {
+            if (ModelState.IsValid)
+            {
+                db.softwares.Add(software);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(software);
+        }
+
+        // GET: softwares/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            software software = db.softwares.Find(id);
+            if (software == null)
+            {
+                return HttpNotFound();
+            }
+            return View(software);
+        }
+
+        // POST: softwares/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,os,programs")] software software)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(software).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(software);
+        }
+
+        // GET: softwares/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            software software = db.softwares.Find(id);
+            if (software == null)
+            {
+                return HttpNotFound();
+            }
+            return View(software);
+        }
+
+        // POST: softwares/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            software software = db.softwares.Find(id);
+            db.softwares.Remove(software);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
